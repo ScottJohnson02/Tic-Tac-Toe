56a005324444f56d36f8ed5365304125cdeef8a0
Scott Johnson <scottyjohnson002@gmail.com>
2019-11-25 17:27:22 -0500

    set up logic for invalid move

--- scripts.js
+++ scripts.js
@@ -1,72 +1,105 @@
-//
-
-let grid = ["", "", "", "", "", "", "", "", "", ];
-let turn = 0;
 /*
 1. create grid
 2. make each item clickable
 3. add an X in the propper array spot
 4. change the text in the clicked item to an x and prevent the box from clicking again
 */
-window.addEventListener("click", event => {
-  if (turn == 0) {
-    divReplace("X");
+let grid = ["", "", "", "", "", "", "", "", "", ];
+let turn = 0;
+let retry = false;
+document.getElementById("grid-container").addEventListener("click", event => {
+  if (turn == 0 && retry == false) {
+    divReplace("X")
     turn++;
-    console.log(turn)
-  } else {
-    divReplace("O");
+  } else if (turn == 1 && retry == false) {
+    divReplace("O")
     turn--;
-    console.log(turn)
   }
+
 });
 
 function divReplace(playerIcon) {
   document.getElementById("item0").addEventListener("click", event => {
-    if (grid[0] == "") {
+    if (grid[0] != "") {
+      retry = true;
+    } else {
       grid[0] = playerIcon;
       document.getElementById("item0").innerHTML = grid[0];
+      retry = false;
     }
-
 
 
   });
   document.getElementById("item1").addEventListener("click", event => {
-    grid[1] = playerIcon;
-    document.getElementById("item1").innerHTML = grid[1];
+    if (grid[1] != "") {
+      retry = true;
+    } else {
+      grid[1] = playerIcon;
+      document.getElementById("item0").innerHTML = grid[1];
+      retry = false;
+    }
   });
   document.getElementById("item2").addEventListener("click", event => {
-    grid[2] = playerIcon;
-
-    document.getElementById("item2").innerHTML = grid[2];
+    if (grid[0] != "") {
+      retry = true;
+    } else {
+      grid[0] = playerIcon;
+      document.getElementById("item0").innerHTML = grid[0];
+      retry = false;
+    }
   });
   document.getElementById("item3").addEventListener("click", event => {
-    grid[3] = playerIcon;
-
-    document.getElementById("item3").innerHTML = grid[3];
+    if (grid[0] != "") {
+      retry = true;
+    } else {
+      grid[0] = playerIcon;
+      document.getElementById("item0").innerHTML = grid[0];
+      retry = false;
+    }
   });
   document.getElementById("item4").addEventListener("click", event => {
-    grid[4] = playerIcon;
-
-    document.getElementById("item4").innerHTML = grid[4];
+    if (grid[0] != "") {
+      retry = true;
+    } else {
+      grid[0] = playerIcon;
+      document.getElementById("item0").innerHTML = grid[0];
+      retry = false;
+    }
   });
   document.getElementById("item5").addEventListener("click", event => {
-    grid[5] = playerIcon;
-
-    document.getElementById("item5").innerHTML = grid[5];
+    if (grid[0] != "") {
+      retry = true;
+    } else {
+      grid[0] = playerIcon;
+      document.getElementById("item0").innerHTML = grid[0];
+      retry = false;
+    }
   });
   document.getElementById("item6").addEventListener("click", event => {
-    grid[6] = playerIcon;
-
-    document.getElementById("item6").innerHTML = grid[6];
+    if (grid[0] != "") {
+      retry = true;
+    } else {
+      grid[0] = playerIcon;
+      document.getElementById("item0").innerHTML = grid[0];
+      retry = false;
+    }
   });
   document.getElementById("item7").addEventListener("click", event => {
-    grid[7] = playerIcon;
-
-    document.getElementById("item7").innerHTML = grid[7];
+    if (grid[0] != "") {
+      retry = true;
+    } else {
+      grid[0] = playerIcon;
+      document.getElementById("item0").innerHTML = grid[0];
+      retry = false;
+    }
   });
   document.getElementById("item8").addEventListener("click", event => {
-    grid[8] = playerIcon;
-
-    document.getElementById("item8").innerHTML = grid[8];
+    if (grid[0] != "") {
+      retry = true;
+    } else {
+      grid[0] = playerIcon;
+      document.getElementById("item0").innerHTML = grid[0];
+      retry = false;
+    }
   });
 }
