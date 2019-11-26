b0501726039cd8198cae7f7639658e6d630bfc0c
Scott Johnson <scottyjohnson002@gmail.com>
2019-11-26 14:06:36 -0500

    add win condition for horizontal

--- scripts.js
+++ scripts.js
@@ -5,22 +5,45 @@
 4. change the text in the clicked item to an x and prevent the box from clicking again
 */
 let turn = 0;
+let grid = ['', '', '', '', '', '', '', '', '']
 
-document.body.addEventListener("click", event =>{
-  if(turn==0){
+
+function winCheck(value) {
+
+  if (grid[0] == value && grid[1] == value && grid[2] == value) {
+    alert(value + " wins")
+  } else if (
+    grid[3] === value && grid[4] === value && grid[5] === value) {
+    alert(value + " wins")
+  } else if (grid[6] === value && grid[7] === value && grid[8] === value) {
+    alert(value + " wins")
+  } else {
+    console.log('no winner')
+  }
+}
+
+document.body.addEventListener("click", event => {
+  if (turn == 0) {
     if (event.target.nodeName == "BUTTON") {
-      if(event.target.textContent == '') {
+      if (event.target.textContent == '') {
         console.log("Clicked", event.target.textContent);
         event.target.textContent = 'X';
+        console.log(event.target.id)
+        grid[event.target.id] = 'X'
+        winCheck('X');
+        console.log('test')
         turn++;
       }
     }
-  }
-  else{
+  } else {
     if (event.target.nodeName == "BUTTON") {
-      if(event.target.textContent == '') {
+
+      if (event.target.textContent == '') {
         console.log("Clicked", event.target.textContent);
         event.target.textContent = 'O';
+        console.log(event.target.id)
+        grid[event.target.id] = 'O';
+        winCheck('O');
         turn--;
       }
     }
